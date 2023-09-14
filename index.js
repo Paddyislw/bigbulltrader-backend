9ffaf5ea4e816117a319373870c4806064390034
Paddyislw <pradyum0701@gmail.com>
2023-09-14 13:07:41 +0530

    added mongo url

--- index.js
+++ index.js
@@ -1,75 +1,78 @@
-import { ObjectId } from 'mongodb';
-import mongoose from 'mongoose';
-import express from 'express';
-import cors from 'cors';
+import { ObjectId } from "mongodb";
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
 
-import image from './models/image.js';
+import image from "./models/image.js";
 
 const app = express();
 
 const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 const PORT = 5000;
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get('/get-all', async function (req, res) {
-    try {
-        res.status(200).json({
-            data: await image.find()
-        });
-    } catch (error) {
-        res.status(500).send(error.toString());
-    }
+app.get("/get-all", async function (req, res) {
+  try {
+    res.status(200).json({
+      data: await image.find(),
+    });
+  } catch (error) {
+    res.status(500).send(error.toString());
+  }
 });
 
-app.post('/save-new', async function (req, res) {
-    try {
-        const { url } = req.body;
+app.post("/save-new", async function (req, res) {
+  try {
+    const { url } = req.body;
 
-        const newImage = new image({ url });
-        await newImage.save();
+    const newImage = new image({ url });
+    await newImage.save();
 
-        res.status(201).send();
-    } catch (error) {
-        res.status(500).send(error.toString());
-    }
+    res.status(201).send();
+  } catch (error) {
+    res.status(500).send(error.toString());
+  }
 });
 
-app.delete('/delete-one/:id', async function (req, res) {
-    try {
-        console.log(req,'req............')
-        await image.deleteOne({ _id: new ObjectId(req.params.id) });
-        res.status(200).send();
-    } catch (error) {
-        res.status(500).send(error.toString());
-    }
-})
+app.delete("/delete-one/:id", async function (req, res) {
+  try {
+    console.log(req, "req............");
+    await image.deleteOne({ _id: new ObjectId(req.params.id) });
+    res.status(200).send();
+  } catch (error) {
+    res.status(500).send(error.toString());
+  }
+});
 
 const startServer = async () => {
+  try {
     try {
-        try {
-            await mongoose.connect('', {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            });
-
-            console.log('Connected to MongoDB');
-        } catch (error) {
-            console.error('Error connecting to MongoDB:', error);
+      await mongoose.connect(
+        "mongodb+srv://Developer:3RwoqNORAnMG8XBG@cluster0.inbyuy1.mongodb.net/?retryWrites=true&w=majority",
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
         }
+      );
 
-        app.listen(PORT, () => {
-            console.log(`Server listening on http://localhost:${PORT}`);
-        });
+      console.log("Connected to MongoDB");
     } catch (error) {
-        console.error('Error starting the server:', error);
+      console.error("Error connecting to MongoDB:", error);
     }
+
+    app.listen(PORT, () => {
+      console.log(`Server listening on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+  }
 };
 
 startServer();
