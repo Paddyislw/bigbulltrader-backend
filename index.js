fe373d2cb33242ddfb35aa843ed4f6057e8f6498
Paddyislw <pradyum0701@gmail.com>
2023-09-14 23:34:02 +0530

    added env file

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors(corsOptions));
 app.use(express.json());
